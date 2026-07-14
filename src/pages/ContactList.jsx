16f41411163e7b6f19d11559279d3ff6c60fb66f
prabhakardevagupthapu24-hub <prabhakar.devagupthapu24@sasi.ac.in>
2026-07-14 13:19:09 +0530

    Update profile image URL in ContactList component

--- src/pages/ContactList.jsx
+++ src/pages/ContactList.jsx
@@ -67,7 +67,7 @@ function ContactList() {
                             <div className="avatar">
                                 {contact.profileImage ? (
                                     <img
-                                        src={`https://contactmanagerbackend-jism.onrender.com${contact.profileImage}`}
+                                        src={`https://contactmanager-manoj-csmb.onrender.com${contact.profileImage}`}
                                         alt={contact.name}
                                         className="avatar-image"/>
                                 ) : (
